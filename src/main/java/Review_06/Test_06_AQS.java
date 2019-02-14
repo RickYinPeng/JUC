0d7df8a3313c12c80de29381eda2031425c91756
RickYinPeng <272940172@qq.com>
2019-02-14 23:20:55 +0800

    JUC中AQS基本了解和原理

---
+++ src/main/java/Review_06/Test_06_AQS.java
@@ -0,0 +1,15 @@
+package Review_06;
+
+import java.util.concurrent.locks.AbstractQueuedLongSynchronizer;
+
+/**
+ * @author RickYinPeng
+ * @ClassName Test_06_AQS
+ * @Description
+ * @date 2019/2/12/19:00
+ */
+public class Test_06_AQS {
+    public static void main(String[] args) {
+        AbstractQueuedLongSynchronizer abstractQueuedLongSynchronizer = null;
+    }
+}
